f155e5adfeb527d71053e5af2830f821b7000c62
Jarek Kajko <jaruto7@gmail.com>
2018-09-18 14:56:01 +0200

    Remove index.js file

--- index.js
+++
@@ -1,8 +0,0 @@
-var http = require( 'http' );
-
-var server = http.createServer();
-server.on( 'request', function( request, response ) {
-    response.write( 'Hello World!' );
-    response.end();
-});
-server.listen( 9000 );
